27b83d586a933789eca545a96676fab6f6e8d005
Andrina05 <andrinarosanne05@gmail.com>
2025-09-04 19:09:21 +0530

    API Integration

--- weatherscript.js
+++ weatherscript.js
@@ -1,69 +1,102 @@
 // A JavaScript code to make the app functional
 
-// Get the two weather items
+// Get the weather items
 const weatherDisplay = document.getElementById('weather-display');
 const weatherForecast = document.getElementById('weather-forecast-info');
 const weatherIndicator = document.getElementById('weather-icon')
 
+// // Path to the JSON file (no longer used; instead, data fetched from API from line 31.)
+// const jsonFilePath = 'testingdata.json';
+
+// // Fetch data from JSON file; to have the app be functional for testing, the Live Server extension for VS Code will be needed.
+// // To test the code in VS Code once the extension has been installed, click on the 'Go Live' button on the bottom right corner.
+// fetch(jsonFilePath).then(response => {
+//     // Give error message if a problem occurs, or else obtain the json file.
+//     if(!response.ok) {
+//         throw new Error(`Oops! Something went wrong. HTTP Error ${response.status}`);
+//     }
+//     return response.json();
+// })
+// .then(data => {
+//     console.log("Data fetched successfully!");
+//     console.log(data);
+//     updateWeatherUI(data);
+// })
+// .catch(error => {
+//     console.log("Error fetching/parsing JSON data:", error);
+// });
+
+
 // URL to be used in final app
 const baseURL = "https://api.open-meteo.com/v1/forecast";
-const weather_params = "&daily=sunrise,sunset,temperature_2m_max,temperature_2m_min,uv_index_max,weather_code,temperature_2m_mean&hourly=temperature_2m,apparent_temperature,precipitation_probability,precipitation,cloud_cover,weather_code&models=best_match&current=temperature_2m,relative_humidity_2m,precipitation,weather_code,apparent_temperature&minutely_15=temperature_2m,precipitation&timezone=auto";
+const weather_params = "daily=sunrise,sunset,temperature_2m_max,temperature_2m_min,temperature_2m_mean,uv_index_max&hourly=temperature_2m,apparent_temperature,precipitation_probability,precipitation,cloud_cover&current=temperature_2m,relative_humidity_2m,apparent_temperature,precipitation,cloud_cover&timezone=auto";
 
-// Path to the JSON file (currently used to test the code)
-const jsonFilePath = 'testingdata.json';
-
-// Fetch data from JSON file; to have the app be functional for testing, the Live Server extension for VS Code will be needed.
-// To test the code in VS Code once the extension has been installed, click on the 'Go Live' button on the bottom right corner.
-fetch(jsonFilePath).then(response => {
-    // Give error message if a problem occurs, or else obtain the json file.
-    if(!response.ok) {
-        throw new Error(`Oops! Something went wrong. HTTP Error ${response.status}`);
-    }
-    return response.json();
-})
-.then(data => {
-    console.log("Data fetched successfully!");
-    console.log(data);
+async function fetchWeatherData(lat, long) {
+    const weatherURL = `${baseURL}?latitude=${lat}&longitude=${long}&${weather_params}`;
+    const response = await fetch(weatherURL);
+    const data = await response.json();
     updateWeatherUI(data);
-})
-.catch(error => {
-    console.log("Error fetching/parsing JSON data:", error);
-});
+}
 
 const updateWeatherUI = data => {
     // Initialize required values
+    const locationInput = document.getElementById('location-input').textContent;
     const weather = data.current;
     const dailyData = data.daily;
     const hourlyData = data.hourly;
 
-    const currentYear = new Date().getFullYear();
-    
-    const currentMonth = new Date().getMonth() + 1;   // getMonth() gives a value from 0 to 11. So add 1 to get the correct month number.
-    const formattedMonth = ((currentMonth < 10) ? '0'+currentMonth : currentMonth);
-
-    const currentDate = new Date().getDate();
-    const formattedDate = ((currentDate < 10) ? '0'+currentDate : currentDate);
-
-    const currentHour = new Date().getHours();
-    const formattedHour = ((currentHour < 10) ? '0'+currentHour : currentHour);
+    console.log("Current:",data.current);
+    console.log("Daily:",data.daily);
+    console.log("Hourly:",data.hourly);
 
     // Match the format in the fetched API JSON data.
-    const roundedTime = `${currentYear}-${formattedMonth}-${formattedDate}T${formattedHour}:00`;
+    let currentTime = weather.time;
+    console.log("Time:", currentTime);
 
-    // For testing, a time is selected from the JSON file.
-    const testingTime = "2025-08-29T22:00";
+    const roundedTime = currentTime.slice(0, 14)+'00';
+    console.log("Rounded Time:", roundedTime);
 
-    const locationInput = document.getElementById('location-input').textContent;
+    const currentTimeIndex = hourlyData.time.indexOf(`${roundedTime}`);
+    console.log("Index of current time:", currentTimeIndex);
 
-    // Get index of current time from the JSON file for now; also defined a 'new hour' to test rounding the hours past midnight
-    const currentTimeIndex = hourlyData.time.indexOf(`${testingTime}`);
-    let newHour = 20;
-    console.log("New hour:", newHour);
+    const currentHour = parseInt(currentTime.slice(11, 13));
+    console.log("Current Hour:", currentHour);
 
-    // Update UI elements in main display
+    // const locTime = weather.time;
+    // console.log("Location Time:",locTime);
+
+    // For testing, a time was selected from the JSON file. It is no longer used.
+    // const testingTime = "2025-08-29T22:00";
+
+    // Obtained index of current time from the JSON file during initial test; also defined a 'new hour' to test rounding the hours past midnight
+    // const currentTimeIndex = hourlyData.time.indexOf(`${testingTime}`);
+    // let newHour = 20;
+    // console.log("New hour:", newHour);
+    
+
+    // Update UI elements
+    // Background colour of the two cards
+    if(currentHour >= 8 && currentHour <= 18) {
+        weatherDisplay.classList.remove('dawndusk', 'night');
+        weatherDisplay.classList.add('day');
+        weatherForecast.classList.remove('dawndusk', 'night');
+        weatherForecast.classList.add('day');
+    }
+    else if ((currentHour >= 5 && currentHour <= 7) || (currentHour >= 18 && currentHour <= 20)) {
+        weatherDisplay.classList.remove('day', 'night');
+        weatherDisplay.classList.add('dawndusk');
+        weatherForecast.classList.remove('day', 'night');
+        weatherForecast.classList.add('dawndusk');        
+    }
+    else if ((currentHour >= 21 && currentHour <= 23) || (currentHour >= 0 && currentHour <= 4)) {
+        weatherDisplay.classList.remove('day', 'dawndusk');
+        weatherDisplay.classList.add('night');
+        weatherForecast.classList.remove('day', 'dawndusk');
+        weatherForecast.classList.add('night');    
+    }
 
     // Weather Icon
-    if ((newHour >= 20 && newHour <= 23) || (newHour >= 0 && newHour <= 5)) {
+    if ((currentHour >= 20 && currentHour <= 23) || (currentHour >= 0 && currentHour <= 5)) {
         if (weather.cloud_cover <= 30 && weather.precipitation >= 0 && weather.precipitation <= 0.4)
             weatherIndicator.innerHTML = `<i class="fas fa-moon" title="Clear"></i>`;
         else if (weather.cloud_cover > 30 && weather.cloud_cover <= 80 && weather.precipitation >= 0 && weather.precipitation <= 0.4)
@@ -73,7 +106,7 @@ const updateWeatherUI = data => {
         else if (weather.precipitation > 0.4)
             weatherIndicator.innerHTML = `<i class="fas fa-cloud-moon-rain" title="Raining"></i>`;
     }
-    else if (newHour > 5 && newHour < 20) {
+    else if (currentHour > 5 && currentHour < 20) {
         if (weather.cloud_cover <= 30 && weather.precipitation >= 0 && weather.precipitation <= 0.4)
             weatherIndicator.innerHTML = `<i class="fas fa-sun" title="Clear"></i>`;
         else if (weather.cloud_cover > 30 && weather.cloud_cover <= 80 && weather.precipitation >= 0 && weather.precipitation <= 0.4)
@@ -102,42 +135,26 @@ const updateWeatherUI = data => {
     document.getElementById('sunrise-time').textContent = new Date(dailyData.sunrise[0]).toLocaleTimeString();
     document.getElementById('sunset-time').textContent = new Date(dailyData.sunset[0]).toLocaleTimeString();
 
-    if(newHour >= 8 && newHour <= 18) {
-        weatherDisplay.classList.remove('dawndusk', 'night');
-        weatherDisplay.classList.add('day');
-        weatherForecast.classList.remove('dawndusk', 'night');
-        weatherForecast.classList.add('day');
-    }
-    else if ((newHour >= 5 && newHour <= 7) || (newHour >= 18 && newHour <= 20)) {
-        weatherDisplay.classList.remove('day', 'night');
-        weatherDisplay.classList.add('dawndusk');
-        weatherForecast.classList.remove('day', 'night');
-        weatherForecast.classList.add('dawndusk');        
-    }
-    else if ((newHour >= 21 && newHour <= 23) || (newHour >= 0 && newHour <= 4)) {
-        weatherDisplay.classList.remove('day', 'dawndusk');
-        weatherDisplay.classList.add('night');
-        weatherForecast.classList.remove('day', 'dawndusk');
-        weatherForecast.classList.add('night');    
-    }
-
+    // Update hourly forecast
     const hourlyForecastContainer = document.getElementById('hourly-forecast');
     hourlyForecastContainer.innerHTML = '';
 
     // Display forecast for five hours after the current time
+    let assignedHour = currentHour;
     for (let i=currentTimeIndex+1; i<=currentTimeIndex+5; i++) {
-        newHour = (newHour + 1) % 24;
+        assignedHour = (assignedHour + 1) % 24;
+        console.log("Assigned Hour:", assignedHour);
         let hourDiv = document.createElement('div');
 
-        if (newHour >= 8 && newHour <= 18) {
+        if (assignedHour >= 8 && assignedHour <= 18) {
             hourDiv.classList.add('light');
             hourDiv.classList.remove('dark', 'moderate');
         }
-        else if ((newHour >= 5 && newHour <= 7) || (newHour >= 18 && newHour <= 20)) {
+        else if ((assignedHour >= 5 && assignedHour <= 7) || (assignedHour >= 18 && assignedHour <= 20)) {
             hourDiv.classList.add('moderate');
             hourDiv.classList.remove('dark', 'light');
         }
-        else if ((newHour >= 21 && newHour <= 23) || (newHour >= 0 && newHour <= 4)) {
+        else if ((assignedHour >= 21 && assignedHour <= 23) || (assignedHour >= 0 && assignedHour <= 4)) {
             hourDiv.classList.add('dark');
             hourDiv.classList.remove('moderate', 'light');
         }
@@ -145,10 +162,48 @@ const updateWeatherUI = data => {
         hourDiv.classList.add('hourly-item');
 
         hourDiv.innerHTML = `
-            <div><b>${newHour}:00</b></div> <br>
+            <div><b>${assignedHour}:00</b></div> <br>
             <div><i class="fas fa-thermometer-half" title="Avg. Temp."></i> ${hourlyData.temperature_2m[i]}°C</div> <br>
             <div><i class="fas fa-cloud-rain" title="Precip. Chance"></i> ${hourlyData.precipitation_probability[i]}%</div>
         `;
         hourlyForecastContainer.appendChild(hourDiv);
     }
 }
+
+
+// Fetch the user's current location
+async function getUserLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(async (userPosition) => {
+            const lat = userPosition.coords.latitude;
+            const long = userPosition.coords.longitude;
+            await fetchWeatherData(lat, long);
+        }, (error) => {
+            alert("Failed to get location. Error:", error);
+        });
+    } else {
+        alert("Geolocation is not supported by the currently used browser.");
+    }
+}
+
+// Function for the 'Get Weather' button
+document.getElementById('weather-fetch-button').addEventListener('click', async () => {
+    const locationInput = document.getElementById('location-input').value.trim();
+    if (locationInput) {
+        const geocodeUrl = `https://geocoding-api.open-meteo.com/v1/search?name=${locationInput}&language=en&count=1`;
+        const response = await fetch(geocodeUrl);
+        const data = await response.json();
+        if (data.results && data.results.length > 0) {
+            const lat = data.results[0].latitude;
+            const lon = data.results[0].longitude;
+            await fetchWeatherData(lat, lon);
+        } else {
+            alert('Location not found');
+        }
+    } else {
+        alert("No data found. Please try again with a valid location.");
+    }
+});
+
+// Upon loading the page, fetch the user's location (the browser may ask permission for the same.)
+window.onload = getUserLocation;
